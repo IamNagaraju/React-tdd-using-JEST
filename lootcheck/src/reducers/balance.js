8eb69d38eed35a80df003cb29230f1b69d60c917
Nagaraju <p.nagaraju@kloctechnologies.co.in>
2019-05-08 18:26:19 +0530

    comments added

--- lootcheck/src/reducers/balance.js
+++ lootcheck/src/reducers/balance.js
@@ -6,7 +6,7 @@ const BALANCE_COOKIE = 'BALANCE_COOKIE';
 const balance = (state = 0, action) => {
   let balance;
 
-  switch(action.type) {
+  switch (action.type) {
     case constants.SET_BALANCE:
       balance = action.balance;
       break;
@@ -17,12 +17,14 @@ const balance = (state = 0, action) => {
       balance = state - action.withdrawal;
       break;
     default:
+      //returns the value of your of your baked cookie.
       balance = parseInt(read_cookie(BALANCE_COOKIE), 10) || state;
   }
-
+  //Bake Cookie allows you to pass a name and a string value to store a cookie on the user's browser. It maps the name to the string
+  // delete_cookie(name) - removes the cookie from the browser history.
   bake_cookie(BALANCE_COOKIE, balance);
 
   return balance;
-}
+};
 
 export default balance;
